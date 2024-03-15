0e62b59953fce4829247ebeaad058375e037c913
Artur-Eric <163476059+Artur-Eric@users.noreply.github.com>
2024-03-14 21:10:28 -0300

    Add files via upload

---
+++ CODIGO DE SOMA DE 5 NUMEROS.c
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+Artur
+
+Somar os 5 numeros
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{    
+   //variaveis
+   int soma = 0 , a , b , c , d , e ;
+   
+   //entradas
+   printf("digite o primeiro valor: ");
+   scanf("%d" , &a);
+   
+   printf("digite o primeiro valor: ");
+   scanf("%d" , &b);
+   
+   printf("digite o primeiro valor: ");
+   scanf("%d" , &c);
+   
+   printf("digite o primeiro valor: ");
+   scanf("%d" , &d);
+   
+   printf("digite o primeiro valor: ");
+   scanf("%d" , &e);
+   
+   //processamento
+   soma = a + b + c + d + e ;
+   
+   //saida
+   printf("a soma desses numeros é %d", soma);
+}
+
+
